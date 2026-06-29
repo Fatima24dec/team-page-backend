c44d6e2c0f8c638c930997f6d4046bcddea3255f
Fatima Al-Ibrahim <fatimaalibrahim010@gmail.com>
2026-06-29 11:06:48 +0300

    add routes for login, dashboard, forgot and reset password

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/forgot_password', function () {
+    return view('forgot_password');
+});
+
+Route::get('/reset_password', function () {
+    return view('reset_password');
+});
+
+
+
